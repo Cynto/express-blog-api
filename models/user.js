013141e4eacf538b9039081627b158fb6e6dd374
Cynto <lucagaravello@outlook.com>
2022-05-02 18:49:15 +0200

    Change from username to email, add first and lastname

--- models/user.js
+++ models/user.js
@@ -2,10 +2,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: {
+  email: {
     type: String,
     required: true,
     unique: true,
+    minlength: 3,
+    maxlength: 15,
+  },
+  fName: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 15,
+  },
+  lName: {
+    type: String,
+    required: true,
     minlength: 3,
     maxlength: 15,
   },
