5d41c7a8248510dec07bfa8cda117e51e86c5d87
Cynto <lucagaravello@outlook.com>
2022-05-02 17:13:46 +0200

    Create post routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/userController');
+const postController = require('../controllers/postController');
 
 /// USER ROUTES ///
 
@@ -19,5 +20,23 @@ router.post('/users/login', userController.user_login_post);
 
 // GET request to logout.
 router.get('/users/logout', userController.user_logout_get);
+
+/// POST ROUTES ///
+
+// POST request for creating Post.
+router.post('/posts', postController.post_create_post);
+
+// GET request for list of all posts.
+router.get('/posts', postController.post_list_get);
+
+// GET request for one post.
+router.get('/posts/:id', postController.post_detail_get);
+
+// PUT request to update post.
+router.put('/posts/:id', postController.post_update_post);
+
+// DELETE request to delete post.
+router.delete('/posts/:id', postController.post_delete_post);
+
 
 module.exports = router;
