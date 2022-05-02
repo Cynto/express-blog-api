0feb657916f00526274e62e85b06a5e319ad7c68
Cynto <lucagaravello@outlook.com>
2022-05-02 18:48:49 +0200

    Create passport JWTstrategy

--- config/passport.js
+++ config/passport.js
@@ -34,6 +34,29 @@ passport.use(
   )
 );
 
+const passportJWT = require('passport-jwt');
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    (jwtPayload, done) => {
+      User.findById(jwtPayload._id, (err, user) => {
+        if (err) {
+          return done(err, false);
+        }
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      });
+    }
+  )
+);
 
 module.exports = passport;
