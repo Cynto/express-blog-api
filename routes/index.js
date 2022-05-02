ce2590e62b4ada41a83d421cba5d5576fe1fb3cb
Cynto <lucagaravello@outlook.com>
2022-05-02 17:06:21 +0200

    Create user routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const userController = require('../controllers/userController');
+
+/// USER ROUTES ///
+
+// POST request for creating User.
+router.post('/users', userController.user_create_post);
+
+// GET request for list of all users.
+router.get('/users', userController.user_list_get);
+
+// GET request for one user.
+router.get('/users/:id', userController.user_detail_get);
+
+// POST request to login.
+router.post('/users/login', userController.user_login_post);
+
+// GET request to logout.
+router.get('/users/logout', userController.user_logout_get);
 
 module.exports = router;
