42b204f8e67e39b996694c3a3dbaefc8974f813a
Cynto <lucagaravello@outlook.com>
2022-08-27 18:08:36 +0200

    Add populated comment objects to post objects

--- controllers/postController.js
+++ controllers/postController.js
@@ -363,6 +363,19 @@ exports.post_detail_get = (req, res, next) => {
           message: 'Post not found.',
         });
       }
+      post.comments = post.comments.map((commentID) => {
+        Comment.findById(commentID)
+          .lean()
+          .populate('user', 'firstName lastName')
+          .exec((err, comment) => {
+            if (err) {
+              return next(err);
+            }
+            if (!comment) {
+              return commentID;
+            }
+          });
+      });
       res.json({ post });
     });
 };
