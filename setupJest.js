a64ea33bb660b60ec60bd50de353dc09b4818f60
Cynto <lucagaravello@outlook.com>
2022-10-21 17:39:31 +0200

    Add and setup test packages Jest, Superagent, supertest, mongodb-memory-server

---
+++ setupJest.js
@@ -0,0 +1,9 @@
+process.env.MONGODB_URI = 'mongodb://localhost:27017/expressBlogAPI';
+process.env.JWT_SECRET = 'secret-key';
+process.env.DEBUG =
+  'userController, commentController, postController, passport';
+process.env.PORT = 4000;
+process.env.ADMIN_CODE = 'idk';
+process.env.IMGUR_CLIENT_ID = 'a102cb0c7100133';
+process.env.BEARER_TOKEN =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc0FkbWluIjpmYWxzZSwiX2lkIjoiNjI3ZTc3ODE3ZGNjZTI2MWI2M2M3N2Q1IiwiZW1haWwiOiJib2JAZ21haWwuY29tIiwiZmlyc3ROYW1lIjoiQm9iIiwibGFzdE5hbWUiOiJCb2JieSIsInBhc3N3b3JkIjoiJDJhJDEwJFZHbk1jRHJtVlJ6Qi83REN4VXprby5wN2ZxZzlsWno3RXE2Sy9ocTY2UWguYVZUc2Vmd0dTIiwiY3JlYXRlZEF0IjoiMjAyMi0wNS0xM1QxNToyMTozNy43MzlaIiwibGFzdExvZ2luIjoiMjAyMi0wNS0xM1QxNToyMTozNy43MzlaIiwiX192IjowLCJpYXQiOjE2NjU1OTA4MzR9.VCYYgrAVlrEPzHWMsMwK5QJ0ovxfw641Iymhf5nNCZE';
